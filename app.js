c73e61ae782ac40dad4e67961fc688b8728621c2
quanpro372 <23050123@student.bdu.edu.vn>
2025-04-12 01:13:05 +0700

    Add files via upload
    Signed-off-by: quanpro372 <23050123@student.bdu.edu.vn>

---
+++ app.js
@@ -0,0 +1,4 @@
+document.getElementById("close-banner-btn").addEventListener("click", function() {
+    const banner = document.getElementById("cookie-banner");
+    banner.style.display = "none"; // Ẩn thanh tác vụ
+  });
